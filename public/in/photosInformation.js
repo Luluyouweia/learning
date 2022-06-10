917b22db76c30e09722e46be241ba4432e83b24a
逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>
2022-06-10 16:14:52 +0000

    update public/in/photosInformation.js.

--- public/in/photosInformation.js
+++ public/in/photosInformation.js
@@ -1,10 +1,10 @@
 		var img=new Array();
-			img=["pictures/main.jpg",//本地
-			"https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1651450661048-be61eba55f1a?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=joshua-lawrence-_GPn7zmkjD4-unsplash.jpg";//链接 链接至  Unplash 图库			"https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1621290102989-e9865bef2dcf?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=vlad-d-nqSeto6vWfU-unsplash.jpg";
-			"https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1649605184337-c4dc52c6e32e?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=parsa-mahmoudi-nfi-TXXzwjI-unsplash.jpg&w=1920";
-			"https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1538428494232-9c0d8a3ab403?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=edward-he-uKyzXEc2k_s-unsplash.jpg&w=2400";
-			"https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1652274801662-25e4ee30534b?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=rogov-ca-XH8HTkT9bX4-unsplash.jpg";
-			]
+			img[0]="pictures/main.jpg";//本地
+			img[1]="https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1651450661048-be61eba55f1a?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=joshua-lawrence-_GPn7zmkjD4-unsplash.jpg";//链接 链接至  Unplash 图库			img[1]
+            img[2]="https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1621290102989-e9865bef2dcf?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=vlad-d-nqSeto6vWfU-unsplash.jpg";
+			img[3]="https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1649605184337-c4dc52c6e32e?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=parsa-mahmoudi-nfi-TXXzwjI-unsplash.jpg&w=1920";
+			img[4]="https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1538428494232-9c0d8a3ab403?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=edward-he-uKyzXEc2k_s-unsplash.jpg&w=2400";
+			img[5]="https://dogefs.s3.ladydaily.com/~/source/unsplash/photo-1652274801662-25e4ee30534b?ixlib=rb-1.2.1&q=80&fmt=jpg&crop=entropy&cs=tinysrgb&dl=rogov-ca-XH8HTkT9bX4-unsplash.jpg";
 			
 		var amusic=new Array(); 
 			amusic[0]="";
