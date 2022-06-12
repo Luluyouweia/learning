68a71aec32255307c16a660a8e1f5cb2721e01de
逯跃翔 <10653661+LU-yuexiang@user.noreply.gitee.com>
2022-06-12 01:36:44 +0000

    update public/in/photosjs.js.

--- public/in/photosjs.js
+++ public/in/photosjs.js
@@ -118,7 +118,7 @@
             }
 
         }
-setInterval("toggleSound()",1
+setInterval("toggleSound()",1);
 		/*
 		function setCookie(cname,cvalue,exdays) { 
 			var d = new Date(); d.setTime(d.getTime()+(exdays*24*60*60*1000)); 
